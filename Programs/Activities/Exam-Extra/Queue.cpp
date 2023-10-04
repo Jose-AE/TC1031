17e223ae28d10ddbd61a50f34028738902af2322
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2023-10-03 22:07:14 -0600

    Removed comments exam act

--- Programs/Activities/Exam-Extra/Queue.cpp
+++ Programs/Activities/Exam-Extra/Queue.cpp
@@ -6,27 +6,15 @@ void Queue::enque(Ticket ticket) {
    Node<Ticket>* newNode = new Node<Ticket>(ticket, nullptr, nullptr);
 
    if (list.getLastNodePtr() == nullptr) {
-      // If the queue is empty, set both first and last nodes to the new node.
       list.setFirstNodePtr(newNode);
       list.setLastNodePtr(newNode);
    } else {
-      // Otherwise, update the next pointer of the last node and set the last
-      // node to the new node.
+
       list.getLastNodePtr()->setNextNodePtr(newNode);
       newNode->setPrevNodePtr(list.getLastNodePtr());
       list.setLastNodePtr(newNode);
    }
 }
-
-// void Queue::enque(Ticket ticket) {
-
-//    if (list.getLastNodePtr() == nullptr) {
-//       list.setFirstNodePtr(new Node<Ticket>(ticket, nullptr, nullptr));
-//       list.setLastNodePtr(new Node<Ticket>(ticket, nullptr, nullptr));
-//    } else {
-//       list.setLastNodePtr(new Node<Ticket>(ticket, temp, nullptr));
-//    }
-// }
 
 Ticket Queue::deque() {
    if (list.getFirstNodePtr() == nullptr) {
